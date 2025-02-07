19dbee998993bb59838e1b8a590e65ca7679800b
mukesh <mukeshvishwakarma251003@gmail.com>
2025-02-07 09:38:15 +0530

    space reduce

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useContext, useState } from 'react'
 import Cookies from "js-cookie"
 export const AuthContext=createContext()
-
 export const AuthProvider=({children})=> {
     const initialUserState = Cookies.get("jwt") || localStorage.getItem("ChatApp");
     // parse the user data and storing in state.
